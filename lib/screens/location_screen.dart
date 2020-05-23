f188d076a057ab92b436d453ad313aef7b05189f
Yao Josué <jyao2000@yahoo.fr>
2020-05-24 01:46:54 +0200

    locationWeather

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -21,7 +21,9 @@ class _LocationScreenState extends State<LocationScreen> {
   }
 
   void updateUI(dynamic weatherData){
-
+    double temperature = weatherData['main']['temp'];
+    int condition = weatherData['weather'][0]['id'];
+    String cityName = weatherData['name'];
   }
 
 
