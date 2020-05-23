0308120535d3da82c2373a84c420819016d566ea
Yao Josué <jyao2000@yahoo.fr>
2020-05-23 19:25:17 +0200

    debut

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 
 
 const String apiKey = "6053fb9ebacfd4868781ad9e892236ee";
@@ -16,35 +17,36 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async{
+  void getLocationData() async{
     Location location = Location();
     await location.getCurrentLocation();
     longitude =location.longitude;
     latitude = location.latitude;
-    print(latitude);
+
+    NetwookHelper netwookHelper = NetwookHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    netwookHelper.getData();
+    double longitude = decodeData['coord']['lon'];
+    double latitude = decodeData['coord']['lat'];
+
     print(longitude);
-    getData();
+
+    String description = decodeData['weather'][0]['description'];
+    int condition = decodeData['weather'][0]['id'];
+
+    double temperature = decodeData['main']['temp'];
+
+    String city = decodeData['name'];
+
+    print(description);
+    print(city);
   }
 
   void getData() async{
-      'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'
-      double longitude = decodeData['coord']['lon'];
-      double latitude = decodeData['coord']['lat'];
 
-      print(longitude);
 
-      String description = decodeData['weather'][0]['description'];
-      int condition = decodeData['weather'][0]['id'];
-
-      double temperature = decodeData['main']['temp'];
-
-      String city = decodeData['name'];
-
-      print(description);
-      print(city);
 
 
   }
